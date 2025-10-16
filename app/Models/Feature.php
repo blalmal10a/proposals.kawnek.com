73d826cfe6e6651111c9f46d274b0cf89a0505c9
blalmal10a <malsawma7777@gmail.com>
2025-10-16 07:41:48 +0000

    feat: update soft delete to the models

--- app/Models/Feature.php
+++ app/Models/Feature.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Feature extends Model
 {
+    use SoftDeletes;
+
     public function feature_group(): BelongsTo
     {
         return $this->belongsTo(FeatureGroup::class);
--- app/Models/FeatureGroup.php
+++ app/Models/FeatureGroup.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FeatureGroup extends Model
 {
+    use SoftDeletes;
+
     public function project(): BelongsTo
     {
         return $this->belongsTo(Project::class);
